e2e0e568412cd05efb4475715f457473b3777437
Khashayar Etemadi Someoliayi <khaes@kth.se>
2021-02-07 23:14:03 +0100

    main2 added

---
+++ src/main/java/Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Thread myThread = new Thread();
+        myThread.run();
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
