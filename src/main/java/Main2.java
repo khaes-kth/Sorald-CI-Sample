1ebe02e8504810395f599e12fff7df3044350099
repairnator <noreply@github.com>
2021-06-09 10:13:00 +0200

    Proposal for patching Sorald rule 1217

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,7 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
         Thread myThread = new Thread();
-        myThread.run();
+        myThread.start();
     }
 
     @Override
